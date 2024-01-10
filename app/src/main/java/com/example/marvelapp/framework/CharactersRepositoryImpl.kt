72e4a749e2bf09852b1c80daa46c4691f8dd4d0a
Luis Ricardo <luiscastrodev@gmail.com>
2024-01-10 14:45:44 -0300

    implementa charactes pagesource

--- app/src/main/java/com/example/marvelapp/framework/CharactersRepositoryImpl.kt
+++ app/src/main/java/com/example/marvelapp/framework/CharactersRepositoryImpl.kt
@@ -5,12 +5,13 @@ import com.example.core.data.repository.CharactersRemoteteDataSource
 import com.example.core.data.repository.CharactersRepository
 import com.example.core.domain.model.Character
 import com.example.marvelapp.framework.network.response.DataWrapperResponse
+import com.example.marvelapp.framework.paging.CharactersPagingSource
 import javax.inject.Inject
 
 class CharactersRepositoryImpl @Inject constructor(
     private val remoteteDataSource: CharactersRemoteteDataSource<DataWrapperResponse>
 ) : CharactersRepository {
     override fun getCharacters(query: String): PagingSource<Int, Character> {
-        TODO()
+        return CharactersPagingSource(remoteteDataSource, query)
     }
 }
